6201abb4fac14e919bcd6a0084fb611c05740d9c
Nolan Buzanis <15nb7@queensu.ca>
2018-05-09 10:18:18 -0400

    Created main.js file and linked to index.html

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if (value) {
+        
+    }
+    else {
+        console.log('Invalid Entry');
+    }
+});
